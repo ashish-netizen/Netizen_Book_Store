73b095995dc0653fac8a212f07a0aea496701282
ashish-netizen <rajawatashish23@gmail.com>
2020-05-30 10:42:01 +0530

    Changes

--- config/prod.py
+++ config/prod.py
@@ -1,5 +1,9 @@
+import os
 DEBUG = False
 SECRET_KEY="topsecret"
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0000@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['Database URl']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
 
